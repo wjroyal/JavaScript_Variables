75d025c84bf2cc917ec7063d1bd71ee0b842c7df
Will Royal <william.j.royal@gmail.com>
2024-10-26 02:21:20 -0700

    init commit

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
+
 
 // Exercise 2
+const firstName = "Will";
+lastName = "Will";
+age = 34;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
